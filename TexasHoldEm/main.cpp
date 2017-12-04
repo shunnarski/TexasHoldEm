b1c70c6b351b60860e8a530929216185233cfa13
alecshunnarah <alecshunnarah@172.17.142.186>
2017-12-04 10:21:15 -0600

    Init commit

---
+++ TexasHoldEm/main.cpp
@@ -0,0 +1,23 @@
+
+/* 
+ * File:   main.cpp
+ * Author: alecshunnarah
+ *
+ * Created on December 4, 2017, 10:18 AM
+ * 
+ * This program is designed to play the game of Texas Hold Em' in a Linux terminal. 
+ * I also wanted to do this to kind of go back over c++, re-learning it in all of its glory.
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+
+    return 0;
+}
+
